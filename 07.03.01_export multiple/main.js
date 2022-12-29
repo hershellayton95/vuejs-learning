b38a8ac59d34f5873aa32ae41570f5510578e4de
hershellayton95 <filidm5795@gmail.com>
2022-12-29 23:15:08 +0100

    slot

--- 07.03.01_export multiple/main.js
+++ 07.03.01_export multiple/main.js
@@ -1,7 +1,11 @@
 import LoginForm from "./LoginForm.js";
 
-const app = Vue.createApp({});
+const app = Vue.createApp({
+    components:{
+        'login-form':LoginForm
+    }
+});
 
-app.component('login-form', LoginForm);
+// app.component('login-form', LoginForm);
 
 app.mount('#app');
---
+++ 09_router/js/hello.js
@@ -0,0 +1,11 @@
+
+export default {
+    data() {
+        return {
+            greeting: 'Hello World!'
+        };
+    },
+    template: `
+        <p>{{ greeting }}</p>
+    `
+}
