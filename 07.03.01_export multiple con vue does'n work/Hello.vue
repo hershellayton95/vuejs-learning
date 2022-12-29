91fd7edb5b6442ddc026ecadb5dba43bf9541cdd
hershellayton95 <filidm5795@gmail.com>
2022-12-29 21:22:45 +0100

    update

---
+++ 07.03.01_export multiple con vue does'n work/Hello.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{hello}}</p>
+</template>
+<script>
+    export default {
+        data(){
+
+            return {
+                hello: "Hello Vue!"
+            }
+        }
+
+    }
+</script>
---
+++ 07.03.01_export multiple con vue does'n work/main.js
@@ -0,0 +1,7 @@
+import LoginForm from "./LoginForm.js";
+
+const app = Vue.createApp({});
+
+app.component('login-form', LoginForm);
+
+app.mount('#app');
